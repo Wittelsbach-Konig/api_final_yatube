bf95b7ac18a2b32133e8c35ad9963dc582a25c64
Кирюшин Виталий <kiryuschin.vitalij@yandex.ru>
2023-06-12 22:32:43 +0300

    Fix PEP8 issues

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import (viewsets,
-                            filters,
-                            mixins,
-                            permissions)
+from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
-from posts.models import (Group,
-                          Post)
-from .serializers import (PostSerializer,
-                          CommentSerializer,
-                          GroupSerializer,
-                          FollowSerializer)
+from posts.models import Group, Post
 from .permissions import AuthorOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
 
 
 class PostViewSet(viewsets.ModelViewSet):
